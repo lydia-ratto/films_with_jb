cf44bd5f77989cf4dcdca85492f37aceac3572d5
Josh Ackroyd <joshua.ackroyd@winnowsolutions.com>
2023-01-14 11:26:55 +0000

    created new seed for development

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -8,11 +8,27 @@ class FilmsController < ApplicationController
     set_film
   end
 
+  def create
+    @film = Deck.new(film_params)
+    if @film.save
+      redirect_to film_path(@deck)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @film = Film.new
+  end
+
   def edit
     set_film
   end
 
+  private
+
   def set_film
     @film = Film.find(params[:id])
   end
+  
 end
